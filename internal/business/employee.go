dfb4469bcfac2a28eaebbf5d78d99aab2a5fda7c
crmix <feruzismoilov10@gmail.com>
2024-11-04 08:19:32 +0500

    cron addition

--- internal/business/employee.go
+++ internal/business/employee.go
@@ -42,7 +42,7 @@ func (s *EmployeeService) SendDailyDutyNotification(bot *tgbotapi.BotAPI) {
 
 	c := cron.New()
 
-	_, err := c.AddFunc("5 4 * * MON-FRI", func() {
+	_, err := c.AddFunc("5 4 * * *", func() {
 		groupId, err := s.RetrievingGroupID()
 		if err != nil {
 			fmt.Printf("error during receiving groupid on api %v", err)
@@ -56,7 +56,15 @@ func (s *EmployeeService) SendDailyDutyNotification(bot *tgbotapi.BotAPI) {
 			log.Printf("Failed to get and update duty employee: %v", err)
 			return
 		}
-		remainderMsg := fmt.Sprintf("Xayrli tong! Bizning bugungi navbatchimiz: <b>%s</b>", employee.Name)
+		remainderMsg := fmt.Sprintf(`<b>Xayrli tong! Bizning bugungi navbatchimiz: <i>%s</i></b> 
+		
+		<b>Navbatchining vazifalari:</b>
+		
+		<b>Idishlarni yig‘ishtirish va tozalash</b> — Ishxona xonasida qolgan idishlarni yig‘ib, yuvilishi kerak bo‘lsa, yuvib qo‘yadi.
+		<b>Zarur joylarni tozalash</b> — Agarda kimdir sizda qasti bo'lib tuzoq qo'ygan bo'lsa topib o'ziga aytib tozalattirish yoki o'zi tozalash.
+		<b>Chiqindilarni tashlash</b> — Kun davomida yig‘ilgan chiqindilarni belgilangan joyga olib chiqib, axlat qutisini yangilaydi.
+		<b>Texnikalarni o‘chirish</b> — Ish vaqti tugagach, kompyuter, printer va boshqa texnikalarni o‘chirish va xavfsizligini ta’minlash.
+	  `, employee.Name)
 		msg := tgbotapi.NewMessage(groupId, remainderMsg)
 		msg.ParseMode = "HTML"
 		_, err = bot.Send(msg)
@@ -91,6 +99,25 @@ func (s *EmployeeService) SendDailyDutyNotification(bot *tgbotapi.BotAPI) {
 	if err != nil {
 		log.Fatalf("Failed to add cron job: %v", err)
 	}
+
+	_, err = c.AddFunc("0 11 * * *", func() {
+		groupId, err := s.RetrievingGroupID()
+		if err != nil {
+		  fmt.Printf("error during retrieving group ID: %v\n", err)
+		  return
+		}
+		resultsMsg := fmt.Sprintf(`<b>Hurmatli hamkasblar!</b>
+		<i>Oshxonamizni yillik muzeyga aylantirmaslik uchun, qolgan ovqatlar va idishlaringizni olib keting. Hidlari bilan san’at asariga aylantirish shart emas!</i>`)
+		msg := tgbotapi.NewMessage(groupId, resultsMsg)
+		msg.ParseMode = "HTML"
+		_, err = bot.Send(msg)
+		if err != nil {
+		  log.Printf("Failed to send results message: %v", err)
+		}
+	  })
+	  if err != nil {
+		log.Fatalf("Failed to add cron job: %v", err)
+	  }
 
 	c.Start()
 
